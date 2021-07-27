25d45d6a031859090ef4915de69f493d83d1c297
SWEDIRE <86052247+SWEDIRE@users.noreply.github.com>
2021-07-27 16:22:22 +0530

    Add files via upload

---
+++ RUNNING-JACKXSON-main (1)/RUNNING-JACKXSON-main/RUNNING-JACKXSON-main/RUNNING-JACKXSON-main/Project_Template_C11_Let-s_Run_JAXON-main/Project_Template_C11_Let-s_Run_JAXON-main/sketch.js
@@ -0,0 +1,36 @@
+var runner, path, lb, rb;
+var runnerImg, pathImg;
+function preload(){
+runnerImg = loadAnimation("Runner-1.png","Runner-2.png","Runner-1.png","Runner-2.png");
+pathImg = loadImage("path.png"); 
+}
+
+function setup(){
+  createCanvas(400,400);
+  path = createSprite(200,200);
+  path.addImage(pathImg);
+  path.velocityY = 4;
+  path.scale = 0.8;
+  lb = createSprite(0,0,100,800);
+  lb.visible = false;
+  rb = createSprite(400,0,100,800);
+  rb.visible = false;
+  runner = createSprite(180,340,30,30);
+  runner.addAnimation("movingRunner",runnerImg);
+  runner.scale = 0.09;
+}
+
+function draw() {
+  background(0);
+  /*path.velocityY = 4;
+  runner.x = World.mouseX;
+  edges = createEdgeSprites();
+  runner.collide(edges[3]);
+  runner.collide(lb);
+  runner.collide(rb);
+
+  if (path.y > 400){
+   path.y = 200;
+  }*/
+  drawSprites();
+}
